79a8156f2e7927ef39e009cf48a54ad8093f40e1
pranavdaa <pranavmaheshwari4321@gmail.com>
2019-03-16 23:04:59 +0530

    Third

--- tictack.sol
+++ tictack.sol
@@ -5,20 +5,35 @@ contract Tiktaktoe {
 
     address[3][3] board;
 
-
+address activePlayer;
     address public player1;
     address public player2;
 
     function TickTacToe() public {
         player1 = msg.sender;
+    activePlayer = player2
     }
     
     function joinGame() public  {
 
          assert(player2 == address(0)); //this is the way to initialise the address so that now no other address can be alloted to join the Game
         player2 = msg.sender;
+        activePlayer = player2
     }
 
+function setStone(uint8 x,uint8 y) public {
+   require(board[x][y] = address(0)); // so that at one box if we set a value we can not change the value in that box
+    assert(x < boardSize)
+    assert(y < boardSize)
+     require(msg.sender = activePlayer)
 
+    board[x][y] = msg.sender;
+    if(activePlayer = 1) {
+        activePlayer = player2
+    }
+    else{
+        activePlayer = player1
+    }
+}
 
 }
